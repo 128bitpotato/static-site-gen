1946c9635743a9b4e94b37f1daea939e277ebf93
128bitpotato <skickligt01.flagig@icloud.com>
2024-11-05 18:20:54 +0100

    TextNode module initialized

---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITAL = "ital"
+    CODE = "code"
+    LINK = "link"
+    IMAG = "imag"
+
+class TextNode():
+    def __init__(text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type.value
+        self.url = url
+    
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return (self.text == other.text and
+                    self.text_type == other.text_type and
+                    self.url == other.url)
+        return False
