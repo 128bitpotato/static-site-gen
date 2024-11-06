a0a8e1d6689f748719535294d6467fa6d7f80790
128bitpotato <skickligt01.flagig@icloud.com>
2024-11-06 06:32:23 +0100

    main function added and tested

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import *
+
+def main():
+
+    test_node = TextNode("Testing the awsome node", TextType.ITAL, url="https://www.boot.dev")
+
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
