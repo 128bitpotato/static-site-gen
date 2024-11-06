b7c0d5b1878cde211c3e59a63ae00f1383027732
128bitpotato <skickligt01.flagig@icloud.com>
2024-11-06 05:49:17 +0100

    __repr__ method added

--- src/textnode.py
+++ src/textnode.py
@@ -20,3 +20,6 @@ class TextNode():
                     self.text_type == other.text_type and
                     self.url == other.url)
         return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
