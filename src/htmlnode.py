43f6bc07f34f3987c8d4e4cb3d202ab40396702d
128bitpotato <skickligt01.flagig@icloud.com>
2024-11-07 20:45:23 +0100

    building html Node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,4 +7,12 @@ class HTMLNode():
         self.children = children
         self.props = props
 
+    def to_html(self):
+        raise NotImplementedError
     
+    def props_to_html(self):
+        list_of_strings = list(map(lambda item: f'{item[0]}="{item[1]}"', self.props.items()))
+        return " " + " ".join(list_of_strings)
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props}"
