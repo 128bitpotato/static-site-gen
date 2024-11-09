f73736fccd7b168884d13840bfa256505759a536
128bitpotato <skickligt01.flagig@icloud.com>
2024-11-09 11:17:01 +0100

    ParentNode added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,3 +29,9 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    
