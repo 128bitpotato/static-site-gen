fb20096b61cfdb12d99c97601759652872c5e539
128bitpotato <skickligt01.flagig@icloud.com>
2024-11-13 21:31:47 +0100

    Added ValueError if unclosed delimiter

--- src/converter_functions.py
+++ src/converter_functions.py
@@ -6,7 +6,9 @@ delimiters = {"**": TextType.BOLD,
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     list_of_nodes = []
     for node in old_nodes:
-        split_list = node.text.split(delimiter)
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("delimiter not closed, invalid number of sections")
 
         starting_type = text_type if node.text.startswith(delimiter) else node.text_type
         type_dict = {"start": starting_type, "other": text_type if starting_type != text_type else node.text_type}
@@ -14,7 +16,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes = list(map(
             lambda num, string: TextNode(string, 
                                 type_dict["start"] if (num % 2 == 0) else type_dict["other"]), 
-                                range(len(split_list)), split_list)) # Range-number of split list to decide node_type
+                                range(len(sections)), sections)) # Range-number of split list to decide node_type
 
         list_of_nodes.extend(new_nodes)
 
