adf11162b8094dbb378f9ae03733b2937a1cfdc9
128bitpotato <skickligt01.flagig@icloud.com>
2024-12-04 07:08:18 +0100

    Initialized copy_content function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,40 @@
 from textnode import *
+import os
+import shutil
 
 def main():
 
-    test_node = TextNode("Testing the awsome node", TextType.ITAL, url="https://www.boot.dev")
+    def copy_content(tree=None, current_dir=None):
 
-    print(test_node)
+        # if os.listdir(path="public") != []:
+        #     if tree == None:
+        #         shutil.rmtree("public")
+        #         os.mkdir("public")
+        
+        if tree is None:
+            tree = []
+
+        current_dir = "static" if current_dir == None else current_dir
+        dir_content = os.listdir(path=current_dir)
+        for i in dir_content:
+            path = os.path.join(current_dir, i)
+            if os.path.isfile(path):
+                tree.append(i)
+            elif os.path.isdir(path):
+                tree.extend([copy_content(tree=None, current_dir=path)])
+
+        return tree
+
+
+
+            
+
+
+        # os.path.exists("static/")
+        # dir = os.listdir(path="static/")
+        return False
+    
+    print(copy_content())
 
 if __name__ == "__main__":
     main()
