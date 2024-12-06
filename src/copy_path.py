74b223c5e0013ec3c2670f18da154f2f6036b828
128bitpotato <skickligt01.flagig@icloud.com>
2024-12-06 05:35:22 +0100

    moved copy_content to separate module

---
+++ src/copy_path.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+def copy_content(current_dir=None):
+
+    if current_dir == None:
+        if os.listdir(path="public") != []:
+            shutil.rmtree("public")
+            os.mkdir("public")
+        
+    tree = []
+
+    current_dir = "static" if current_dir == None else current_dir
+    dir_content = os.listdir(path=current_dir)
+
+    destination_path = os.path.join("public", strip_static_path(current_dir))
+
+    for i in dir_content:
+        path = os.path.join(current_dir, i)
+        if os.path.isfile(path):
+            shutil.copy(path, destination_path)
+            tree.append(i)
+        elif os.path.isdir(path):
+            destination_folder = os.path.join(destination_path, i)
+            os.mkdir(destination_folder)
+            tree.extend([copy_content(current_dir=path)])
+
+    return tree
+
+def strip_static_path(path):
+    if path.startswith("static"):
+        return "/".join(path.split("/")[1:])
+    return path
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import re
 
+from copy_path import copy_content
+
 def extract_title(markdown):
     title = re.match(r"(^#{1}\s)(.*)$", markdown, re.M)
     if title != None:
@@ -11,36 +13,7 @@ def extract_title(markdown):
 
 def main():
 
-    def copy_content(current_dir=None):
-
-        if current_dir == None:
-            if os.listdir(path="public") != []:
-                shutil.rmtree("public")
-                os.mkdir("public")
-        
-        tree = []
-
-        current_dir = "static" if current_dir == None else current_dir
-        dir_content = os.listdir(path=current_dir)
-
-        destination_path = os.path.join("public", strip_static_path(current_dir))
-
-        for i in dir_content:
-            path = os.path.join(current_dir, i)
-            if os.path.isfile(path):
-                shutil.copy(path, destination_path)
-                tree.append(i)
-            elif os.path.isdir(path):
-                destination_folder = os.path.join(destination_path, i)
-                os.mkdir(destination_folder)
-                tree.extend([copy_content(current_dir=path)])
-
-        return tree
-
-    def strip_static_path(path):
-        if path.startswith("static"):
-            return "/".join(path.split("/")[1:])
-        return path
+    # copy_content()
 
     def generate_page(from_path, template_path, dest_path):
         print(f"Generating page from {from_path} to {dest_path} using {template_path}")
